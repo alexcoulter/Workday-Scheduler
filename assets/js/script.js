1c64765b9e61d9210efa50c0adf31c4e19422f59
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-01-27 11:31:39 -0500

    first commit of working application

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+//document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function(){
+
+    
+    
+    //recalls user inputed schedule from local storage and puts it in the correct hour
+    for(var i = 1; i < 10; i++) {
+      $("#textArea" + i).text(localStorage.getItem("hour" + i));
+    }
+
+
+//gets date and time from moment.js and displays them in the header
+var date = moment().format('MMMM Do YYYY');
+$("#date").text(date);
+$("#time").text(moment().format('h:mm: a'));
+
+
+//updates the time every 10 seconds to keep it accurate
+setInterval(function() {
+  var time = moment().format('h:mm: a');
+  $("#time").text(time);
+}, 10000);
+
+
+
+//submit button click event and storing calendar in local storage
+   $("button").on("click", function() {
+      var userText =  $(this).parent().siblings().children("textarea").val();
+      var hourId = $(this).attr("id");
+      localStorage.setItem(hourId, userText);
+
+   });
+
+   //Changes color of text box depending on if that time is in the future, past, or present hour
+   var hour = parseFloat(moment().format('h'));  
+   var amOrPm = moment().format('a');
+   console.log(hour, amOrPm);
+   if (amOrPm === "pm" && hour !== 12) {                 //sets the hour to army time so we can compare to our scheduler
+     hour += 12;                                    
+   }
+   else if (amOrPm === "am" && hour === 12) {
+     hour = 0; 
+   }
+   
+   for(var i = 1; i < 10; i++) {
+      if(hour === i + 8) {                            //add 8 to i since hour1 is 9am (1 + 8 = 9)
+        $("#textArea" + i).addClass("present");
+      }
+      if(hour < i + 8 ) {
+        $("#textArea" + i).addClass("future");
+      }
+     
+   }
+   
+
+
+});
