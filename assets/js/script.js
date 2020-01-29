7fa063e4c3cd20b0801e22424989b4437cc8a97c
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-01-29 13:46:59 -0500

    added warning modal for clearing schedule and changed color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,69 @@
-//document.addEventListener("DOMContentLoaded", function () {
-  $(document).ready(function(){
-
-    
-    
-    //recalls user inputed schedule from local storage and puts it in the correct hour
-    for(var i = 1; i < 10; i++) {
-      $("#textArea" + i).text(localStorage.getItem("hour" + i));
-    }
+$(document).ready(function () {
 
 
-//gets date and time from moment.js and displays them in the header
-var date = moment().format('MMMM Do YYYY');
-$("#date").text(date);
-$("#time").text(moment().format('h:mm: a'));
+
+  //recalls user inputed schedule from local storage and puts it in the correct hour
+  for (var i = 1; i < 10; i++) {
+    $("#textArea" + i).text(localStorage.getItem("hour" + i));
+  }
 
 
-//updates the time every 10 seconds to keep it accurate
-setInterval(function() {
+  //gets date and time from moment.js and displays them in the header
+  var date = moment().format('MMMM Do YYYY');
   var time = moment().format('h:mm: a');
-  $("#time").text(time);
-  colorTextBoxes();
-
-}, 10000);
+  $("#date").text(date + "  /  " + time);
 
 
+  //updates the time every 10 seconds to keep it accurate
+  setInterval(function () {
+    var date = moment().format('MMMM Do YYYY');
+    var time = moment().format('h:mm: a');
+    $("#date").text(date + "  /  " + time);
+    colorTextBoxes();
 
-//submit button click event and storing calendar in local storage
-   $("button").on("click", function() {
-      var userText =  $(this).parent().siblings().children("textarea").val();
-      var hourId = $(this).attr("id");
-      localStorage.setItem(hourId, userText);
+  }, 10000);
 
-   });
 
-   
-    //Clear button clears the whole scheduler
-   $("#clearButton").on("click", function(){
-     console.log("hey");
+
+  //submit button click event and storing calendar in local storage
+  $(".saveButton").on("click", function () {
+    var userText = $(this).parent().siblings().children("textarea").val();
+    var hourId = $(this).attr("id");
+    localStorage.setItem(hourId, userText);
+
+  });
+
+
+  //Clear button clears the whole scheduler
+  $("#clearButton").on("click", function () {
     localStorage.clear();
-    for(var i = 1; i < 10; i++) {
+    for (var i = 1; i < 10; i++) {
       $("#textArea" + i).val("");
     }
 
-   });
- //Changes color of text box depending on if that time is in the future, past, or present hour
- function colorTextBoxes() {
- var hour = parseFloat(moment().format('h'));  
- var amOrPm = moment().format('a');
- console.log(hour, amOrPm);
- if (amOrPm === "pm" && hour !== 12) {                 //sets the hour to army time so we can compare to our scheduler
-   hour += 12;                                    
- }
- else if (amOrPm === "am" && hour === 12) {
-   hour = 0; 
- }
- 
- for(var i = 1; i < 10; i++) {
-    if(hour === i + 8) {                            //add 8 to i since hour1 is 9am (1 + 8 = 9)
-      $("#textArea" + i).addClass("present");
+  });
+  //Changes color of text box depending on if that time is in the future, past, or present hour
+  function colorTextBoxes() {
+    var hour = parseFloat(moment().format('h'));
+    var amOrPm = moment().format('a');
+
+    if (amOrPm === "pm" && hour !== 12) {                 //sets the hour to army time so we can compare to our scheduler
+      hour += 12;
     }
-    if(hour < i + 8 ) {
-      $("#textArea" + i).addClass("future");
+    else if (amOrPm === "am" && hour === 12) {
+      hour = 0;
     }
-   
- }
- }
-colorTextBoxes();
+
+    for (var i = 1; i < 10; i++) {
+      if (hour === i + 8) {                            //add 8 to i since hour1 is 9am (1 + 8 = 9)
+        $("#textArea" + i).addClass("present");
+      }
+      if (hour < i + 8) {
+        $("#textArea" + i).addClass("future");
+      }
+
+    }
+  }
+  colorTextBoxes();
 
 });
